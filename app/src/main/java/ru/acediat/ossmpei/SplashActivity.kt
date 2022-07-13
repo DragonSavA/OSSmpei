fd948ab2da7cd6c83cb2e169c0d35e759fb5f0b5
AldrFo <mister.fo1996@gmail.com>
2022-07-14 02:43:42 +0300

    Добавил splashscreen

---
+++ app/src/main/java/ru/acediat/ossmpei/SplashActivity.kt
@@ -0,0 +1,28 @@
+package ru.acediat.ossmpei
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.acediat.feature_auth.AuthActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    //TODO: сделать подгрузку из preferences настоящего положения дел
+    private val isAuth = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if(isAuth)
+            startMainActivity()
+        else
+            startAuthActivity()
+
+        finish()
+    }
+
+    private fun startMainActivity() = startActivity(Intent(this, MainActivity::class.java))
+
+    private fun startAuthActivity() = startActivity(Intent(this, AuthActivity::class.java))
+}
